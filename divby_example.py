f4ce6e281d63622fe5a2966e41ee945e45629e03
ileleiwi <ileleiwi@gmail.com>
2020-12-04 12:26:55 -0700

    added try and except statement to handle divide by zero error

--- divby_example.py
+++ divby_example.py
@@ -1,6 +1,10 @@
 def div42by(divideBy):
-    return 42 / divideBy
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: You tried to divide by zero.')
 
+    
 print(div42by(2))
 print(div42by(12))
 print(div42by(0))
