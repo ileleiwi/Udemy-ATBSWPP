cb02012476de3a66d1ee89a94808a3d4eb0a1b44
ileleiwi <ileleiwi@gmail.com>
2020-12-04 12:33:39 -0700

    added handling of negative numbers

--- numCats.py
+++ numCats.py
@@ -1,7 +1,9 @@
 print('How many cats do you have?')
 numCats = input()
 try:
-    if int(numCats) >= 4:
+    if int(numCats) <= 0:
+        print("You should get a cat")
+    elif int(numCats) >= 4:
         print('That is a lot of cats.')
     else:
         print('That is not that many cats.')
